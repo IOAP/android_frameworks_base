cf545b142a539199d7a61cb49f144f37dc3b6377
xplodwild <xplod@ouverta.fr>
2013-09-03 20:55:56 +0200

    Port MultiWindow from OMNI
    Change-Id: Iccb52eb172664bca816fc6e2b11b5b8dcf1e8f4c

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -3531,6 +3531,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             }
         }
     }
+    
+    public Rect getContentRect() {
+        return new Rect(mContentLeft, mContentTop, mContentRight, mContentBottom);
+    }
 
     /** {@inheritDoc} */
     @Override
