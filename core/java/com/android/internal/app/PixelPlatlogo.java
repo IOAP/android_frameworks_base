9186fc08d38d54bda5c6ada7d210b706ac1e87b2
Arnav Gosain <nolinuxnoparty@gmail.com>
2014-01-01 18:08:03 +0530

    update pixelplatlogo.java

--- core/java/com/android/internal/app/PixelPlatlogo.java
+++ core/java/com/android/internal/app/PixelPlatlogo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010 The Android Open Source Project
+ * Copyright (C) 2010 The PixelROM Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,7 +40,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class PlatLogoActivity extends Activity {
+public class PixelPlatlogo extends Activity {
     FrameLayout mContent;
     int mCount;
     final Handler mHandler = new Handler();
@@ -49,130 +49,130 @@ public class PlatLogoActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-
-        Typeface bold = Typeface.create("sans-serif", Typeface.BOLD);
-        Typeface light = Typeface.create("sans-serif-light", Typeface.NORMAL);
-
-        mContent = new FrameLayout(this);
-        mContent.setBackgroundColor(0xC0000000);
-        
-        final FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(
-                FrameLayout.LayoutParams.WRAP_CONTENT,
-                FrameLayout.LayoutParams.WRAP_CONTENT);
-        lp.gravity = Gravity.CENTER;
-
-        final ImageView logo = new ImageView(this);
-        logo.setImageResource(com.android.internal.R.drawable.platlogo);
-        logo.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
-        logo.setVisibility(View.INVISIBLE);
-
-        final View bg = new View(this);
-        bg.setBackgroundColor(BGCOLOR);
-        bg.setAlpha(0f);
-
-        final TextView letter = new TextView(this);
-
-        letter.setTypeface(bold);
-        letter.setTextSize(300);
-        letter.setTextColor(0xFFFFFFFF);
-        letter.setGravity(Gravity.CENTER);
-        letter.setText(String.valueOf(Build.ID).substring(0, 1));
-
-        final int p = (int)(4 * metrics.density);
-
-        final TextView tv = new TextView(this);
-        if (light != null) tv.setTypeface(light);
-        tv.setTextSize(30);
-        tv.setPadding(p, p, p, p);
-        tv.setTextColor(0xFFFFFFFF);
-        tv.setGravity(Gravity.CENTER);
-        tv.setTransformationMethod(new AllCapsTransformationMethod(this));
-        tv.setText("PixelROM 1.0 - Android " + Build.VERSION.RELEASE);
-        tv.setVisibility(View.INVISIBLE);
-
-        mContent.addView(bg);
-        mContent.addView(letter, lp);
-        mContent.addView(logo, lp);
-
-        final FrameLayout.LayoutParams lp2 = new FrameLayout.LayoutParams(lp);
-        lp2.gravity = Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL;
-        lp2.bottomMargin = 10*p;
-
-        mContent.addView(tv, lp2);
-
-        mContent.setOnClickListener(new View.OnClickListener() {
-            int clicks;
-            @Override
-            public void onClick(View v) {
-                clicks++;
-                if (clicks >= 6) {
-                    mContent.performLongClick();
-                    return;
+    
+            DisplayMetrics metrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(metrics);
+    
+            Typeface bold = Typeface.create("sans-serif", Typeface.BOLD);
+            Typeface light = Typeface.create("sans-serif-light", Typeface.NORMAL);
+    
+            mContent = new FrameLayout(this);
+            mContent.setBackgroundColor(0xC0000000);
+            
+            final FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(
+                    FrameLayout.LayoutParams.WRAP_CONTENT,
+                    FrameLayout.LayoutParams.WRAP_CONTENT);
+            lp.gravity = Gravity.CENTER;
+    
+            final ImageView logo = new ImageView(this);
+            logo.setImageResource(com.android.internal.R.drawable.platlogo);
+            logo.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+            logo.setVisibility(View.INVISIBLE);
+    
+            final View bg = new View(this);
+            bg.setBackgroundColor(BGCOLOR);
+            bg.setAlpha(0f);
+    
+            final TextView letter = new TextView(this);
+    
+            letter.setTypeface(bold);
+            letter.setTextSize(300);
+            letter.setTextColor(0xFFFFFFFF);
+            letter.setGravity(Gravity.CENTER);
+            letter.setText(String.valueOf(Build.ID).substring(0, 1));
+    
+            final int p = (int)(4 * metrics.density);
+    
+            final TextView tv = new TextView(this);
+            if (light != null) tv.setTypeface(light);
+            tv.setTextSize(30);
+            tv.setPadding(p, p, p, p);
+            tv.setTextColor(0xFFFFFFFF);
+            tv.setGravity(Gravity.CENTER);
+            tv.setTransformationMethod(new AllCapsTransformationMethod(this));
+            tv.setText("PixelROM 1.0 - Android " + Build.VERSION.RELEASE);
+            tv.setVisibility(View.INVISIBLE);
+    
+            mContent.addView(bg);
+            mContent.addView(letter, lp);
+            mContent.addView(logo, lp);
+    
+            final FrameLayout.LayoutParams lp2 = new FrameLayout.LayoutParams(lp);
+            lp2.gravity = Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL;
+            lp2.bottomMargin = 10*p;
+    
+            mContent.addView(tv, lp2);
+    
+            mContent.setOnClickListener(new View.OnClickListener() {
+                int clicks;
+                @Override
+                public void onClick(View v) {
+                    clicks++;
+                    if (clicks >= 6) {
+                        mContent.performLongClick();
+                        return;
+                    }
+                    letter.animate().cancel();
+                    final float offset = (int)letter.getRotation() % 360;
+                    letter.animate()
+                        .rotationBy((Math.random() > 0.5f ? 360 : -360) - offset)
+                        .setInterpolator(new DecelerateInterpolator())
+                        .setDuration(700).start();
                 }
-                letter.animate().cancel();
-                final float offset = (int)letter.getRotation() % 360;
-                letter.animate()
-                    .rotationBy((Math.random() > 0.5f ? 360 : -360) - offset)
-                    .setInterpolator(new DecelerateInterpolator())
-                    .setDuration(700).start();
-            }
-        });
-
-        mContent.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                if (logo.getVisibility() != View.VISIBLE) {
-                    bg.setScaleX(0.01f);
-                    bg.animate().alpha(1f).scaleX(1f).setStartDelay(500).start();
-                    letter.animate().alpha(0f).scaleY(0.5f).scaleX(0.5f)
-                            .rotationBy(360)
-                            .setInterpolator(new AccelerateInterpolator())
-                            .setDuration(1000)
+            });
+    
+            mContent.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    if (logo.getVisibility() != View.VISIBLE) {
+                        bg.setScaleX(0.01f);
+                        bg.animate().alpha(1f).scaleX(1f).setStartDelay(500).start();
+                        letter.animate().alpha(0f).scaleY(0.5f).scaleX(0.5f)
+                                .rotationBy(360)
+                                .setInterpolator(new AccelerateInterpolator())
+                                .setDuration(1000)
+                                .start();
+                        logo.setAlpha(0f);
+                        logo.setVisibility(View.VISIBLE);
+                        logo.setScaleX(0.5f);
+                        logo.setScaleY(0.5f);
+                        logo.animate().alpha(1f).scaleX(1f).scaleY(1f)
+                            .setDuration(1000).setStartDelay(500)
+                            .setInterpolator(new AnticipateOvershootInterpolator())
                             .start();
-                    logo.setAlpha(0f);
-                    logo.setVisibility(View.VISIBLE);
-                    logo.setScaleX(0.5f);
-                    logo.setScaleY(0.5f);
-                    logo.animate().alpha(1f).scaleX(1f).scaleY(1f)
-                        .setDuration(1000).setStartDelay(500)
-                        .setInterpolator(new AnticipateOvershootInterpolator())
-                        .start();
-                    tv.setAlpha(0f);
-                    tv.setVisibility(View.VISIBLE);
-                    tv.animate().alpha(1f).setDuration(1000).setStartDelay(1000).start();
+                        tv.setAlpha(0f);
+                        tv.setVisibility(View.VISIBLE);
+                        tv.animate().alpha(1f).setDuration(1000).setStartDelay(1000).start();
+                        return true;
+                    }
+                    return false;
+                }
+            });
+    
+            logo.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    if (Settings.System.getLong(getContentResolver(), Settings.System.EGG_MODE, 0)
+                            == 0) {
+                        // For posterity: the moment this user unlocked the easter egg
+                        Settings.System.putLong(getContentResolver(),
+                                Settings.System.EGG_MODE,
+                                System.currentTimeMillis());
+                    }
+                    try {
+                        startActivity(new Intent(Intent.ACTION_MAIN)
+                            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                                | Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+                            .addCategory("com.android.internal.category.PLATLOGO"));
+                    } catch (ActivityNotFoundException ex) {
+                        android.util.Log.e("PixelPlatlogo", "Couldn't catch a break.");
+                    }
+                    finish();
                     return true;
                 }
-                return false;
-            }
-        });
-
-        logo.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                if (Settings.System.getLong(getContentResolver(), Settings.System.EGG_MODE, 0)
-                        == 0) {
-                    // For posterity: the moment this user unlocked the easter egg
-                    Settings.System.putLong(getContentResolver(),
-                            Settings.System.EGG_MODE,
-                            System.currentTimeMillis());
-                }
-                try {
-                    startActivity(new Intent(Intent.ACTION_MAIN)
-                        .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                            | Intent.FLAG_ACTIVITY_CLEAR_TASK
-                            | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
-                        .addCategory("com.android.internal.category.PLATLOGO"));
-                } catch (ActivityNotFoundException ex) {
-                    android.util.Log.e("PlatLogoActivity", "Couldn't catch a break.");
-                }
-                finish();
-                return true;
-            }
-        });
-        
-        setContentView(mContent);
+            });
+            
+            setContentView(mContent);
+        }
     }
-}
